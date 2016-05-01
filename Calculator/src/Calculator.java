c7f8184d6a4235ebe4a117722bbfec4774b7eebe
QCC2 <awesomedude1210@gmail.com>
2016-04-30 21:18:07 -0400

    Button Function Finished

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,4 +1,5 @@
 import java.awt.Font;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,7 +18,7 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 public class Calculator {
-
+	private int count = 0, change = 0;
 	protected Shell shell;
 
 	/**
@@ -69,56 +70,60 @@ public class Calculator {
 		layOver.setAlignment(SWT.RIGHT);
 		layOver.setBounds(12, 36, 798, 47);
 		
+		Label memoryStatus = new Label(shell, SWT.NONE);
+		memoryStatus.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.NORMAL));
+		memoryStatus.setAlignment(SWT.CENTER);
+		memoryStatus.setBackground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_LIGHT_SHADOW));
+		memoryStatus.setBounds(12, 104, 43, 26);
+		
 		Button degreesCircle = new Button(shell, SWT.RADIO);
 		degreesCircle.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+				change = 1;
 			}
 		});
-		degreesCircle.setBounds(12, 163, 87, 43);
+		degreesCircle.setBounds(141, 163, 87, 43);
 		degreesCircle.setText("Degrees");
 		
 		Button radiansCircle = new Button(shell, SWT.RADIO);
 		radiansCircle.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+				change = 0;
 			}
 		});
-		radiansCircle.setBounds(157, 161, 78, 47);
+		radiansCircle.setBounds(23, 161, 78, 47);
 		radiansCircle.setText("Radians");
+		radiansCircle.setSelection(true);
 		
-		Button gradsCircle = new Button(shell, SWT.RADIO);
-		gradsCircle.addSelectionListener(new SelectionAdapter() {
+		Button memorySubtract = new Button(shell, SWT.NONE);
+		memorySubtract.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+				memoryStatus.setText("M");
+				CalculatorFunctions.memorySubtract(Double.parseDouble(numberInput.getText()));
 			}
 		});
-		gradsCircle.setBounds(306, 165, 78, 39);
-		gradsCircle.setText("Grads");
+		memorySubtract.setBounds(741, 163, 69, 43);
+		memorySubtract.setText("M-");
 		
-		Button memoryMinus = new Button(shell, SWT.NONE);
-		memoryMinus.addSelectionListener(new SelectionAdapter() {
+		Button memoryAdd = new Button(shell, SWT.NONE);
+		memoryAdd.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				
+				memoryStatus.setText("M");
+				CalculatorFunctions.memoryAdd(Double.parseDouble(numberInput.getText()));
 			}
 		});
-		memoryMinus.setBounds(741, 163, 69, 43);
-		memoryMinus.setText("M-");
-		
-		Button memoryPlus = new Button(shell, SWT.NONE);
-		memoryPlus.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-			}
-		});
-		memoryPlus.setBounds(660, 163, 69, 43);
-		memoryPlus.setText("M+");
+		memoryAdd.setBounds(660, 163, 69, 43);
+		memoryAdd.setText("M+");
 		
 		Button memoryRecall = new Button(shell, SWT.NONE);
 		memoryRecall.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.getMemory()));
 			}
 		});
 		memoryRecall.setBounds(498, 163, 69, 43);
@@ -128,20 +133,39 @@ public class Calculator {
 		memoryClear.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+				CalculatorFunctions.setMemory(0);
 			}
 		});
 		memoryClear.setBounds(417, 163, 69, 43);
 		memoryClear.setText("MC");
 		
 		Button memoryStore = new Button(shell, SWT.NONE);
+		memoryStore.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.getMemory()));
+			}
+		});
 		memoryStore.setBounds(579, 163, 69, 43);
 		memoryStore.setText("MS");
 		
 		Button secondRoot = new Button(shell, SWT.NONE);
+		secondRoot.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.secondRoot(Double.parseDouble(numberInput.getText()))));
+			}
+		});
 		secondRoot.setBounds(741, 219, 69, 43);
 		secondRoot.setText("\u221A");
 		
 		Button plusMinus = new Button(shell, SWT.NONE);
+		plusMinus.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.plusMinus(Double.parseDouble(numberInput.getText()))));
+			}
+		});
 		plusMinus.setBounds(660, 219, 69, 43);
 		plusMinus.setText("\u00B1");
 		
@@ -177,43 +201,109 @@ public class Calculator {
 		clearEntry.setBounds(498, 219, 69, 43);
 		clearEntry.setText("CE");
 		
-		Button rightPar = new Button(shell, SWT.NONE);
-		rightPar.setBounds(336, 218, 69, 43);
-		rightPar.setText(")");
+		Button eFour = new Button(shell, SWT.NONE);
+		eFour.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.ePower(4)));
+			}
+		});
+		eFour.setBounds(336, 218, 69, 43);
+		eFour.setText("e\u2074");
 		
-		Button leftPar = new Button(shell, SWT.NONE);
-		leftPar.setBounds(255, 218, 69, 43);
-		leftPar.setText("(");
+		Button eThree = new Button(shell, SWT.NONE);
+		eThree.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.ePower(3)));
+			}
+		});
+		eThree.setBounds(255, 218, 69, 43);
+		eThree.setText("e\u00B3");
 		
 		Button eTwo = new Button(shell, SWT.NONE);
+		eTwo.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.ePower(2)));
+			}
+		});
 		eTwo.setBounds(174, 219, 69, 43);
 		eTwo.setText("e\u00B2");
 		
 		Button eOne = new Button(shell, SWT.NONE);
+		eOne.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.ePower(1)));
+			}
+		});
 		eOne.setBounds(93, 219, 69, 43);
 		eOne.setText("e");
 		
 		Button naturalLog = new Button(shell, SWT.NONE);
+		naturalLog.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.naturalLog(Double.parseDouble(numberInput.getText()))));
+			}
+		});
 		naturalLog.setBounds(12, 219, 69, 43);
 		naturalLog.setText("ln");
 		
-		Button Int = new Button(shell, SWT.NONE);
-		Int.setBounds(12, 275, 69, 43);
-		Int.setText("Int");
+		Button binary = new Button(shell, SWT.NONE);
+		binary.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(CalculatorFunctions.binary(Integer.parseInt(numberInput.getText())));
+			}
+		});
+		binary.setBounds(12, 275, 69, 43);
+		binary.setText("Bin");
 		
 		Button sine = new Button(shell, SWT.NONE);
+		sine.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(change == 1)
+					numberInput.setText(String.valueOf(CalculatorFunctions.sine(Double.parseDouble(numberInput.getText()),1)));
+				else
+					numberInput.setText(String.valueOf(CalculatorFunctions.sine(Double.parseDouble(numberInput.getText()),0)));
+			}
+		});
 		sine.setBounds(93, 275, 69, 43);
-		sine.setText("sine");
+		sine.setText("sin");
 		
 		Button sineInverse = new Button(shell, SWT.NONE);
+		sineInverse.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(change == 1)
+					numberInput.setText(String.valueOf(CalculatorFunctions.sineInverse(Double.parseDouble(numberInput.getText()),1)));
+				else
+					numberInput.setText(String.valueOf(CalculatorFunctions.sineInverse(Double.parseDouble(numberInput.getText()),0)));
+			}
+		});
 		sineInverse.setBounds(174, 276, 69, 43);
 		sineInverse.setText("sin\u207B\u00B9");
 		
 		Button xSecond = new Button(shell, SWT.NONE);
+		xSecond.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.xPower(Double.parseDouble(numberInput.getText()),2)));
+			}
+		});
 		xSecond.setBounds(255, 276, 69, 43);
 		xSecond.setText("\u00D7\u00B2");
 		
 		Button xFactorial = new Button(shell, SWT.NONE);
+		xFactorial.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.xFactorial(Double.parseDouble(numberInput.getText()))));
+			}
+		});
 		xFactorial.setBounds(336, 275, 69, 43);
 		xFactorial.setText("x!");
 		
@@ -260,34 +350,90 @@ public class Calculator {
 		divide.setText("/");
 		
 		Button percent = new Button(shell, SWT.NONE);
+		percent.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				CalculatorFunctions.setOperation("percent");
+				layOver.setText(numberInput.getText() + "%");
+				numberInput.setText("");
+			}
+		});
 		percent.setBounds(741, 275, 69, 43);
 		percent.setText("%");
 		
-		Button binary = new Button(shell, SWT.NONE);
-		binary.setBounds(12, 331, 69, 43);
-		binary.setText("Bin");
+		Button hexadecimal = new Button(shell, SWT.NONE);
+		hexadecimal.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(CalculatorFunctions.hexadecimal(Integer.parseInt(numberInput.getText())));
+			}
+		});
+		hexadecimal.setBounds(12, 331, 69, 43);
+		hexadecimal.setText("Hex");
 		
 		Button cosine = new Button(shell, SWT.NONE);
+		cosine.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(change == 1)
+					numberInput.setText(String.valueOf(CalculatorFunctions.cosine(Double.parseDouble(numberInput.getText()),1)));
+				else
+					numberInput.setText(String.valueOf(CalculatorFunctions.cosine(Double.parseDouble(numberInput.getText()),0)));
+			}
+		});
 		cosine.setBounds(93, 331, 69, 43);
 		cosine.setText("cos");
 		
 		Button cosineInverse = new Button(shell, SWT.NONE);
+		cosineInverse.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(change == 1)
+					numberInput.setText(String.valueOf(CalculatorFunctions.cosineInverse(Double.parseDouble(numberInput.getText()),1)));
+				else
+					numberInput.setText(String.valueOf(CalculatorFunctions.cosineInverse(Double.parseDouble(numberInput.getText()),0)));
+			}
+		});
 		cosineInverse.setBounds(174, 331, 69, 43);
 		cosineInverse.setText("cos\u207B\u00B9");
 		
 		Button xThird = new Button(shell, SWT.NONE);
+		xThird.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.xPower(Double.parseDouble(numberInput.getText()),3)));
+			}
+		});
 		xThird.setBounds(255, 331, 69, 43);
 		xThird.setText("\u00D7\u00B3");
 		
 		Button thirdRoot = new Button(shell, SWT.NONE);
+		thirdRoot.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.thirdRoot(Double.parseDouble(numberInput.getText()))));
+			}
+		});
 		thirdRoot.setBounds(336, 331, 69, 43);
 		thirdRoot.setText("\u221B");
 		
-		Button fourthRoot = new Button(shell, SWT.NONE);
-		fourthRoot.setBounds(336, 387, 69, 43);
-		fourthRoot.setText("\u221C");
+		Button random = new Button(shell, SWT.NONE);
+		random.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.random()));
+			}
+		});
+		random.setBounds(336, 387, 69, 43);
+		random.setText("rand");
 		
 		Button xFourth = new Button(shell, SWT.NONE);
+		xFourth.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.xPower(Double.parseDouble(numberInput.getText()),4)));
+			}
+		});
 		xFourth.setBounds(255, 387, 69, 43);
 		xFourth.setText("x\u2074");
 		
@@ -364,6 +510,12 @@ public class Calculator {
 		multiply.setText("*");
 		
 		Button oneOverX = new Button(shell, SWT.NONE);
+		oneOverX.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.oneOverX(Double.parseDouble(numberInput.getText()))));
+			}
+		});
 		oneOverX.setBounds(741, 331, 69, 43);
 		oneOverX.setText("1/x");
 		
@@ -405,45 +557,108 @@ public class Calculator {
 		equals.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				double result = CalculatorFunctions.computation(layOver.getText().substring(0,layOver.getText().length()-1),numberInput.getText());
+				double result = 0;
+				if(count == 1)
+					result = CalculatorFunctions.computation(layOver.getText().substring(0,layOver.getText().length()-3),numberInput.getText());
+				else
+					result = CalculatorFunctions.computation(layOver.getText().substring(0,layOver.getText().length()-1),numberInput.getText());
+				
 				CalculatorFunctions.setOperation("");
 				layOver.setText("");
 				numberInput.setText(String.valueOf(result));
+				count = 0;
 				
 			}
 		});
 		equals.setBounds(741, 387, 69, 99);
 		equals.setText("=");
 		
-		Button dms = new Button(shell, SWT.NONE);
-		dms.setBounds(12, 387, 69, 43);
-		dms.setText("dms");
+		Button octal = new Button(shell, SWT.NONE);
+		octal.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(CalculatorFunctions.octal(Integer.parseInt(numberInput.getText())));
+			}
+		});
+		octal.setBounds(12, 387, 69, 43);
+		octal.setText("Oct");
 		
 		Button pi = new Button(shell, SWT.NONE);
+		pi.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.pi()));
+			}
+		});
 		pi.setBounds(12, 443, 69, 43);
 		pi.setText("\u03C0");
 		
 		Button tangentInverse = new Button(shell, SWT.NONE);
+		tangentInverse.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(change == 1)
+					numberInput.setText(String.valueOf(CalculatorFunctions.tangentInverse(Double.parseDouble(numberInput.getText()),1)));
+				else
+					numberInput.setText(String.valueOf(CalculatorFunctions.tangentInverse(Double.parseDouble(numberInput.getText()),0)));
+			}
+		});
 		tangentInverse.setBounds(174, 387, 69, 43);
 		tangentInverse.setText("tan\u207B\u00B9");
 		
 		Button tangent = new Button(shell, SWT.NONE);
+		tangent.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(change == 1)
+					numberInput.setText(String.valueOf(CalculatorFunctions.tangent(Double.parseDouble(numberInput.getText()),1)));
+				else
+					numberInput.setText(String.valueOf(CalculatorFunctions.tangent(Double.parseDouble(numberInput.getText()),0)));
+			}
+		});
 		tangent.setBounds(93, 387, 69, 43);
 		tangent.setText("tan");
 		
 		Button tau = new Button(shell, SWT.NONE);
+		tau.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.tau()));
+			}
+		});
 		tau.setBounds(93, 443, 69, 43);
 		tau.setText("\u03C4");
 		
-		Button mod = new Button(shell, SWT.NONE);
-		mod.setBounds(174, 443, 69, 43);
-		mod.setText("Mod");
+		Button modulus = new Button(shell, SWT.NONE);
+		modulus.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				count = 1;
+				CalculatorFunctions.setOperation("modulus");
+				layOver.setText(numberInput.getText() + "mod");
+				numberInput.setText("");
+			}
+		});
+		modulus.setBounds(174, 443, 69, 43);
+		modulus.setText("Mod");
 		
 		Button log = new Button(shell, SWT.NONE);
+		log.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.log(Double.parseDouble(numberInput.getText()))));
+			}
+		});
 		log.setBounds(255, 443, 69, 43);
 		log.setText("log");
 		
 		Button tenToTheX = new Button(shell, SWT.NONE);
+		tenToTheX.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numberInput.setText(String.valueOf(CalculatorFunctions.tenToTheX(Double.parseDouble(numberInput.getText()))));
+			}
+		});
 		tenToTheX.setBounds(336, 443, 69, 43);
 		tenToTheX.setText("10^x");
 		
@@ -456,9 +671,5 @@ public class Calculator {
 		});
 		zero.setBounds(417, 443, 150, 43);
 		zero.setText("0");
-		
-
-		
-
 	}
 }
--- Calculator/src/CalculatorFunctions.java
+++ Calculator/src/CalculatorFunctions.java
@@ -1,6 +1,7 @@
 public class CalculatorFunctions {
 	
 public static String operation;
+public static double memory = 0;
 
 public static double computation(String num1, String num2){
 	double val1 = Double.parseDouble(num1), val2 = Double.parseDouble(num2), result = 0;
@@ -12,6 +13,10 @@ public static double computation(String num1, String num2){
 	 result = multiply(val1,val2);
 	else if(operation.equals("divide"))
 		 result = divide(val1,val2);
+	else if(operation.equals("percent"))
+		 result = percent(val1,val2);
+	else if(operation.equals("modulus"))
+		 result = modulus(val1,val2);
 	return result;
 }
 
@@ -21,6 +26,22 @@ public static void setOperation(String set){
 
 public static String getOperation(){
 	return operation;
+}
+
+public static void setMemory(double set){
+	memory = set;
+}
+
+public static double getMemory(){
+	return memory;
+}
+
+public static void memoryAdd(double num){
+	memory += num;
+}
+
+public static void memorySubtract(double num){
+	memory -= num;
 }
 
 public static double add(double num1, double num2){
@@ -37,5 +58,123 @@ public static double multiply(double num1, double num2){
 
 public static double divide(double num1, double num2){
 	return num1 / num2;
+}
+
+public static double oneOverX(double num){
+	return 1.0 / num;
+}
+
+public static double percent(double num1, double num2){
+	return (num1 * num2) / 100.0;
+}
+
+public static double modulus(double num1, double num2){
+	return num1%num2;
+}
+
+public static double plusMinus(double num){
+	return -1.0*num;
+}
+
+public static double secondRoot(double num){
+	return Math.sqrt(num);
+}
+
+public static double thirdRoot(double num){
+	return Math.cbrt(num);
+}
+
+public static double ePower(int power){
+	return Math.pow(Math.E, power);
+}
+
+public static double xPower(double num, int power){
+	return Math.pow(num, power);
+}
+
+public static double naturalLog(double num){
+	return Math.log(num);
+}
+
+public static double log(double num){
+	return Math.log10(num);
+}
+
+public static double xFactorial(double num){
+	int result = 1;
+	for(int r=1; r<= num; r++){
+		result *= r;
+	}
+	return result;
+}
+
+public static int random(){
+	return 1 + (int)(Math.random()*100);
+}
+
+public static double tenToTheX(double num){
+	return Math.pow(10, num);
+}
+
+public static String binary(int num){
+	return Integer.toBinaryString(num);
+}
+
+public static String hexadecimal(int num){
+	return Integer.toHexString(num);
+}
+
+public static String octal(int num){
+	return Integer.toOctalString(num);
+}
+
+public static double sine(double num, int type){
+	if(type==1)
+		return Math.toDegrees(Math.sin(num));
+	else
+		return Math.sin(num);
+}
+
+public static double cosine(double num, int type){
+	if(type==1)
+		return Math.toDegrees(Math.cos(num));
+	else
+		return Math.cos(num);
+}
+
+public static double tangent(double num, int type){
+	if(type==1)
+		return Math.toDegrees(Math.tan(num));
+	else
+		return Math.tan(num);
+}
+
+public static double sineInverse(double num, int type){
+	if(type==1)
+		return Math.toDegrees(Math.asin(num));
+	else
+		return Math.asin(num);
+}
+
+public static double cosineInverse(double num, int type){
+	if(type==1)
+		return Math.toDegrees(Math.acos(num));
+	else
+		return Math.acos(num);
+}
+
+public static double tangentInverse(double num, int type){
+	if(type==1)
+		return Math.toDegrees(Math.atan(num));
+	else
+		return Math.tan(num);
+}
+
+public static double pi(){
+	return Math.PI;
+}
+
+public static double tau(){
+	return (2.0*Math.PI);
 }
 }
